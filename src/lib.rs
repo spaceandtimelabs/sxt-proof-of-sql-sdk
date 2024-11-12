8e2a6341c8f92d4e36cd329feecacd6fe52854ee
Trevor Lovell <trevor.lovell@spaceandtime.io>
2024-11-12 12:52:56 -0700

    fix: import prover_query module in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ mod substrate;
 pub use substrate::query_commitments;
 mod sxt_chain_runtime;
 
+mod prover_query;
+pub use prover_query::{plan_prover_query_dory, PlanProverQueryError};
+
 mod prover {
     tonic::include_proto!("sxt.core");
 }
