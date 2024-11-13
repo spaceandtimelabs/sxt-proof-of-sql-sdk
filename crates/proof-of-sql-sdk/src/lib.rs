e71fb1ce6f281560120a1125b513a236aa93e9fa
Trevor Lovell <trevor.lovell@spaceandtime.io>
2024-11-12 18:44:31 -0700

    feat: minimize dependencies of sdk in the wasm case

--- crates/proof-of-sql-sdk/src/lib.rs
+++ crates/proof-of-sql-sdk/src/lib.rs
@@ -1,10 +1,20 @@
+#[cfg(feature = "client")]
 mod auth;
+#[cfg(feature = "client")]
 pub use auth::get_access_token;
+
+#[cfg(feature = "client")]
 mod client;
+#[cfg(feature = "client")]
 pub use client::{PostprocessingLevel, SxTClient};
+
+#[cfg(feature = "client")]
 mod substrate;
+#[cfg(feature = "client")]
 pub use substrate::query_commitments;
-pub mod sxt_chain_runtime;
+
+#[cfg(feature = "client")]
+mod sxt_chain_runtime;
 
 mod prover_query;
 pub use prover_query::{plan_prover_query_dory, PlanProverQueryError};
@@ -12,6 +22,12 @@ pub use prover_query::{plan_prover_query_dory, PlanProverQueryError};
 mod verify;
 pub use verify::{verify_prover_response, VerifyProverResponseError};
 
+#[cfg(feature = "client")]
 mod prover {
     tonic::include_proto!("sxt.core");
+}
+
+#[cfg(not(feature = "client"))]
+mod prover {
+    tonic::include_proto!("sxt.messages");
 }
