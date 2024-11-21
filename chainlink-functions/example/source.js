f5a6582b4f438b1c1c086a956e6ebfb85a526140
Jay White <JayWhite2357@users.noreply.github.com>
2024-11-21 00:21:56 -0500

    fix: update code to use imports properly
    Additionally, some code is refactored to simplify the usage of the SDK
    
    Note: there is a build_index.sh script that must be run to build the artifacts needed to import. This script is temporary and should be replace by a CI job that publishes to npm.

--- chainlink-functions/example/source.js
+++ chainlink-functions/example/source.js
@@ -1,27 +1,33 @@
 // Import the package
-const SxTProofModule = await import("npm:sxt-chain-sdk");
-
-// Extract the default export (SxTProof class)
-const SxTProof = SxTProofModule.default;
+const SxTSDK = await import(
+  "https://raw.githubusercontent.com/spaceandtimelabs/sxt-proof-of-sql-sdk/feat/import-sdk/chainlink-functions/source/index.js"
+);
 
 // Define test parameters
-const queryString = 'SELECT SUM(BLOCK_NUMBER), COUNT(*) FROM ETHEREUM.BLOCKS';
+const queryString = "SELECT SUM(BLOCK_NUMBER), COUNT(*) FROM ETHEREUM.BLOCKS";
+const table = "ETHEREUM.BLOCKS";
+// TODO: This is currently hardcoded. But, we need to make it dynamic.
 const commitmentKey =
-    '0xca407206ec1ab726b2636c4b145ac28749505e273536fae35330b966dac69e86a4832a125c0464e066dd20add960efb518424c4f434b5320455448455245554d4a9e6f9b8d43f6ad008f8c291929dee201';
+  "0xca407206ec1ab726b2636c4b145ac28749505e273536fae35330b966dac69e86a4832a125c0464e066dd20add960efb518424c4f434b5320455448455245554d4a9e6f9b8d43f6ad008f8c291929dee201";
 
 if (!secrets.apiKey) {
-    throw Error("Missing secret: apiKey");
+  throw Error("Missing secret: apiKey");
 }
 
 // Initialize the SxTProof instance
-const proof = new SxTProof(queryString, commitmentKey, secrets.apiKey);
+const proof = new SxTSDK.SxTClient(
+  "https://api.spaceandtime.dev/v1/prove",
+  "https://proxy.api.spaceandtime.dev/auth/apikey",
+  "https://rpc.testnet.sxt.network/",
+  secrets.apiKey,
+);
 
 try {
-    // Kick off the proof and await execution
-    const result = await proof.executeWorkflow();
-    console.log("Workflow completed successfully:", result);
-    return Functions.encodeString("Verified");
+  // Kick off the proof and await execution
+  const result = await proof.queryAndVerify(queryString, table, commitmentKey);
+  console.log("Workflow completed successfully:", result);
+  return Functions.encodeString("Verified");
 } catch (error) {
-    console.log("Workflow failed: ", error);
-    return Functions.encodeString("Failed: ", error);
+  console.log("Workflow failed: ", error);
+  return Functions.encodeString("Failed: ", error);
 }
