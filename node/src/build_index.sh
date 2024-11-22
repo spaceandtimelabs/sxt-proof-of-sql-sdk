d1225a2fe45b18148bc59c6ee83ef52b02e769ea
dustin.ray <dustin.vaughan@spaceandtime.io>
2024-11-22 14:29:56 -0800

    fix: refactor

--- node/src/build_index.sh
+++ node/src/build_index.sh
@@ -74,3 +74,9 @@ main() {
 }
 
 main "$@"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+curl $1/sxt_proof_of_sql_sdk_wasm.js | cat - ./index_tail.js > ./index.js
+curl $1/sxt_proof_of_sql_sdk_wasm_bg.wasm > ./sxt_proof_of_sql_sdk_wasm_bg.wasm
