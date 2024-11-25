d48a9e4ae0a039b472e4a365f35a6fccf87fdd46
zachary frederick <zachfred62@gmail.com>
2024-11-25 12:29:33 -0500

    fix: formatting

--- crates/proof-of-sql-sdk/src/substrate.rs
+++ crates/proof-of-sql-sdk/src/substrate.rs
@@ -67,8 +67,8 @@ pub async fn query_commitments(
 }
 
 /// Errors that may occur during the attestation process.
-#[allow(dead_code)] 
 #[derive(Debug, Snafu)]
+#[allow(dead_code)]
 pub enum AttestationError {
     /// Represents an error originating from the Subxt library.
     #[snafu(display("Subxt error: {source}"))]
