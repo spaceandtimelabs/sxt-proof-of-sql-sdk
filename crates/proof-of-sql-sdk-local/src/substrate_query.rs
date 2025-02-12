f994fbd6347a43e9391eae6844152f108a585f6a
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-02-12 16:06:05 -0500

    feat: bump Rust to 1.84.0 and proof-of-sql to 0.72.7

--- crates/proof-of-sql-sdk-local/src/substrate_query.rs
+++ crates/proof-of-sql-sdk-local/src/substrate_query.rs
@@ -1,17 +1,21 @@
+use super::DEFAULT_SCHEMA;
 use crate::sxt_chain_runtime::api::runtime_types::{
     bounded_collections::bounded_vec::BoundedVec, sxt_core::tables::TableIdentifier,
 };
-use proof_of_sql_parser::{Identifier, ResourceId};
+use proof_of_sql::base::database::TableRef;
+use sqlparser::ast::Ident;
 
-/// Convert PoSQL `Identifier` to SxT Core `BoundedVec<u8>`
-fn identifier_to_byte_string(identifier: &Identifier) -> BoundedVec<u8> {
-    BoundedVec::<u8>(identifier.as_str().to_uppercase().as_bytes().to_vec())
+/// Convert sqlparser `Ident` to SxT Core `BoundedVec<u8>`
+fn ident_to_byte_string(ident: &Ident) -> BoundedVec<u8> {
+    BoundedVec::<u8>(ident.value.as_str().to_uppercase().as_bytes().to_vec())
 }
 
-/// Convert PoSQL resource IDs to SxT Core table identifiers
-pub fn resource_id_to_table_id(resource_id: &ResourceId) -> TableIdentifier {
+/// Convert PoSQL `TableRef` to SxT Core table identifiers
+pub fn table_ref_to_table_id(table_ref: &TableRef) -> TableIdentifier {
+    let default_schema_ident = Ident::new(DEFAULT_SCHEMA);
+    let namespace_ident = table_ref.schema_id().unwrap_or(&default_schema_ident);
     TableIdentifier {
-        name: identifier_to_byte_string(&resource_id.object_name()),
-        namespace: identifier_to_byte_string(&resource_id.schema()),
+        name: ident_to_byte_string(table_ref.table_id()),
+        namespace: ident_to_byte_string(namespace_ident),
     }
 }
